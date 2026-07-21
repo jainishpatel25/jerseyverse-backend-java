63d7aa0f3d8ebb23843fa048cb6f2da8c7523fd3
jainishpatel25 <jainish637@gmail.com>
2026-07-21 20:46:26 +0530

    feat: dashboard implementation

--- src/main/java/com/ecommerce/jerseyverse/controller/admin/AdminDashboardController.java
+++ src/main/java/com/ecommerce/jerseyverse/controller/admin/AdminDashboardController.java
@@ -1,7 +1,14 @@
 package com.ecommerce.jerseyverse.controller.admin;
 
+import com.ecommerce.jerseyverse.dto.response.dashboard.DashboardResponse;
 import com.ecommerce.jerseyverse.service.admin.DashboardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/admin/dashboard")
 public class AdminDashboardController {
 
     private final DashboardService dashboardService;
@@ -10,4 +17,8 @@ public class AdminDashboardController {
         this.dashboardService = dashboardService;
     }
 
+    @GetMapping
+    public ResponseEntity<DashboardResponse> getDashboardOverview() {
+        return ResponseEntity.ok(dashboardService.getDashboardOverview());
+    }
 }
